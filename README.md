550b896876013844e777c446070891d76a6a1383
domae <domae@cv.info.gifu-u.ac.jp>
2019-07-12 08:25:16 +0900

    first commit :bird:

---
+++ README.md
@@ -0,0 +1,13 @@
+# Graph Based Gloal Reasoning Network(GloRe)
+
+[GloRe](https://arxiv.org/abs/1811.12814) implementation in Pytorch.  
+
+# Environments
+
+python >= 3.5  
+pytorch >= 1.0.1  
+
+# Usage
+
+Please put "GloRe Module" anywhere you like :good:  
+
---
+++ glore.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+
+
+class GloRe(nn.Module):
+    def __init__(self,channel):
+        super(GloRe, self).__init__()
+        # node num      : int(channel/4))
+        # node channel  : int(channel)
+        self.reduce_dim = nn.Conv2d(channel,int(channel), kernel_size=1, stride=1, bias=False)
+        self.create_projection_matrix = nn.Conv2d(channel,int(channel/4), kernel_size=1, stride=1, bias=False)
+        self.GCN_step_1 = nn.Conv1d(in_channels=int(channel),out_channels=int(channel),kernel_size=3, stride=1,padding=1)
+        self.GCN_step_2 = nn.Conv1d(in_channels=int(channel/4),out_channels=int(channel/4),kernel_size=3, stride=1,padding=1)
+        self.expand_dim = nn.Conv2d(int(channel),channel, kernel_size=1, stride=1, bias=False)    
+
+    def forward(self, x):
+        x_ = self.reduce_dim(x)
+        B = self.create_projection_matrix(x)
+        # reduce dim
+        b,c,h,w = x_.size()
+        x_ = x_.view(b,c,h*w)
+        # projection matrix
+        b,c,h,w = B.size()
+        B = B.view(b,c,h*w)
+        # b,N,L -> b,L,N
+        B = torch.transpose(B, 2, 1)
+        # coordinate space -> latent relation space
+        V=torch.matmul(x_,B)
+        # GCN_1-1
+        V1 = self.GCN_step_1(V)
+        V1 = torch.transpose(V1, 2, 1)
+        # GCN1-2
+        V2 = self.GCN_step_2(V1)
+        V2 = torch.transpose(V2, 2, 1)
+        # Reverse Projection Matrix
+        B = torch.transpose(B, 2, 1)
+        # latent relation space -> coordinate space
+        Y=torch.matmul(V2,B)
+        b,c,_ = Y.size()
+        # b,c,numpix -> b,c,h,w
+        Y = Y.view(b,c,h,w)
+        #self.expand_dim
+        Y = self.expand_dim(Y)
+        
+        x = x + Y
+        return x
